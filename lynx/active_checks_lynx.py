9e2a0106824db8a454e4e909772429089c4b057f
Robert Sander <gurubert@gurubert.de>
2013-07-31 19:25:07 +0200

    added license stanza

--- lynx/active_checks_lynx.py
+++ lynx/active_checks_lynx.py
@@ -6,6 +6,17 @@
 #          Robert Sander <r.sander@heinlein-support.de>
 #
 
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  This file is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
 group = "activechecks"
 
 register_rule(group,
