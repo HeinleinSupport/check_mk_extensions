bfcc57ec465ccfe9b72bb9357c7d5d1315613442
Robert Sander <r.sander@heinlein-support.de>
2013-10-10 16:55:04 +0200

    Check Postfix Configuration Variables

---
+++ postconf/web/plugins/wato/check_parameters_postconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+group = "checkparams"
+
+subgroup_applications = _("Applications, Processes &amp; Services")
+
+register_check_parameters(
+    subgroup_applications,
+    "postconf",
+    _("Postfix Configuration Settings"),
+    ListOf(
+        Tuple(
+            show_titles = True,
+            orientation = "horizontal",
+            elements = [
+                TextAscii( title = _("Name of Configuration Variable"), ),
+                TextAscii( title = _("Expected Value"), ),
+                ]
+            ),
+        add_label = _("Add Variable"),
+        help = _('The check <tt>postconf</tt> monitors the Postfix configuration. Every configuration variable can be checked against a specific value.'),
+    ),
+    None,
+    None
+)
+
