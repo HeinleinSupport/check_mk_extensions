ffb587647c53bfe251a663119d760ff95e4a4fd3
Robert Sander <r.sander@heinlein-support.de>
2016-01-13 17:48:35 +0100

    Now with agent bakery

---
+++ sslcertificates/web/plugins/wato/agent_bakery_sslcertificates.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# group = "agents/" + _("Agent Plugins")
+group = "agents/" + _("Test Heinlein")
+
+register_rule(group,
+    "agent_config:sslcertificates",
+    Alternative(
+        title = _("SSL Certificates"),
+        help = _("This will deploy the agent plugin <tt>sslcertificates</tt> "
+                 "for checking SSL certificate files. <b>Note:</b> If you want "
+                 "to configure several directories to look into for SSL certificate "
+                 "files, then simply create several rules. In this ruleset "
+                 "<b>all</b> matching rules "
+                 "are being executed, not only the first one. "),
+        style = "dropdown",
+        elements = [
+            Dictionary(
+                title = _("Deploy the SSL certificates plugin"),
+                elements = [
+                   ( "directories",
+                     ListOfStrings(
+                        title = _("Directories to look into for SSL certificate files"),
+                        valuespec = TextAscii(
+                            size = 80,
+                            regex = "^/[^ \t]+$",
+                            regex_error = _("Directory paths must begin with <tt>/</tt> and must not contain spaces."),
+                       ),
+                       allow_empty = False,
+
+                     )
+                   ),
+                ],
+                optional_keys = False,
+            ),
+            FixedValue(None, title = _("Do not deploy the SSL certificates plugin"), totext = _("(disabled)") ),
+        ]
+    ),
+    match = "all",
+)
+
