1b42fd880e025846e0360f1d5ceef9418484c060
Robert Sander <r.sander@heinlein-support.de>
2016-01-13 16:34:36 +0100

    Now with agent bakery support

---
+++ entropy_avail/web/plugins/wato/agent_bakery_entropy_avail.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+group = "agents/" + _("Agent Plugins")
+
+register_rule(group,
+    "agent_config:entropy_avail",
+    DropdownChoice(
+        title = _("Entropy Available (Linux)"),
+        help = _("This will deploy the agent plugin <tt>entropy_avail</tt> for monitoring the available entropy."),
+        choices = [
+            ( True, _("Deploy plugin for Entropy Check") ),
+            ( None, _("Do not deploy plugin for Entropy Check") ),
+        ]
+    )
+)
+
