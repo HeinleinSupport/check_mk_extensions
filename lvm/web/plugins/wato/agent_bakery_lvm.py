4cd2040a0f271603f6e3d2337d5d70a9f70af101
Robert Sander <r.sander@heinlein-support.de>
2016-01-14 14:49:57 +0100

    Now with agent bakery

---
+++ lvm/web/plugins/wato/agent_bakery_lvm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+group = "agents/" + _("Agent Plugins")
+
+register_rule(group,
+    "agent_config:lvm",
+    DropdownChoice(
+        title = _("LVM Status (Linux)"),
+        help = _("This will deploy the agent plugin <tt>lvm</tt> for monitoring the status of LVM PVs and VGs."),
+        choices = [
+            ( True, _("Deploy plugin for LVM") ),
+            ( None, _("Do not deploy plugin for LVM") ),
+        ]
+    )
+)
+
