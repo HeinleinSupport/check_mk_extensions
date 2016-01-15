a8bf07a6c356893f95eb64584879017d71467f1d
Robert Sander <r.sander@heinlein-support.de>
2016-01-15 13:01:24 +0100

    Now with agent bakery

---
+++ ox/web/plugins/wato/agent_bakery_ox_runtimestats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+group = "agents/" + _("Agent Plugins")
+
+register_rule(group,
+    "agent_config:ox_runtimestats",
+    DropdownChoice(
+        title = _("Open-Xchange runtime statistics (Linux)"),
+        help = _("This will deploy the agent plugin <tt>ox_runtimestats</tt> for monitoring Open-Xchange."),
+        choices = [
+            ( True, _("Deploy plugin for Open-Xchange") ),
+            ( None, _("Do not deploy plugin for Open-Xchange") ),
+        ]
+    )
+)
+
