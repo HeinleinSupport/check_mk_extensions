6273d390c150cca67ed9158514ed6c115bb3bb97
Robert Sander <r.sander@heinlein-support.de>
2016-10-31 12:30:17 +0100

    make package name an argument of the script

--- helper/copy_package_files.sh
+++ helper/copy_package_files.sh
@@ -2,7 +2,7 @@
 
 # copy the files from a package to the CWD
 
-mkp list dovereplstat | while read file; do
+mkp list "$1" | while read file; do
 
   lfile=${file#$OMD_ROOT/local/share/check_mk}
   ldir=.$(dirname $lfile)
