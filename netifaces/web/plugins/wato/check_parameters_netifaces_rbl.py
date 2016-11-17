fe6f189292b5dd04bdab7a4fa0ccde95c4621413
Robert Sander <r.sander@heinlein-support.de>
2016-11-17 14:59:40 +0100

    RBL checks now with warn and crit lists

--- netifaces/web/plugins/wato/check_parameters_netifaces_rbl.py
+++ netifaces/web/plugins/wato/check_parameters_netifaces_rbl.py
@@ -5,16 +5,30 @@ register_check_parameters(
     subgroup_applications,
     "netifaces_rbl",
     _("RBL"),
-    ListOfStrings(
+    Dictionary(
         title = _("List of RBLs to check against"),
-        add_label = _("Add RBL"),
         help = _('The check <tt>netifaces.rbl</tt> monitors IP addresses of the host against the RBLs defined here.'),
+        elements = [
+            ( 'warn',
+                ListOfStrings(
+                    title = _("WARN"),
+                    help = _('This list contains the RBLs that generate a WARNING state.'),
+                ),
+            ),
+            ( 'crit',
+                ListOfStrings(
+                    title = _('CRIT'),
+                    help = _('This list contains the RBLs that generate a CRITICAL state.'),
+                    default_value = ['ix.dnsbl.manitu.net', 'bl.spamcop.net', 'zen.spamhaus.org'],
+                ),
+            ),
+        ],
     ),
     TextAscii(
         title = _("Interface Address"),
         help = _("The IP address as returned by the netifaces agent plugin."),
         allow_empty = False
     ),
-    None
+    match_type = 'dict',
 )
 
