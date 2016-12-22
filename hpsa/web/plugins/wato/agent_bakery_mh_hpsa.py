3db582dea341207b366454d99dd00dcd953254bd
Robert Sander <r.sander@heinlein-support.de>
2016-12-22 17:34:22 +0100

    Now with interval for the agent plugin

--- hpsa/web/plugins/wato/agent_bakery_mh_hpsa.py
+++ hpsa/web/plugins/wato/agent_bakery_mh_hpsa.py
@@ -5,12 +5,22 @@ group = "agents/" + _("Agent Plugins")
 
 register_rule(group,
     "agent_config:mh_hpsa",
-    DropdownChoice(
+    Alternative(
         title = _("HP RAID Controller Status (Linux)"),
         help = _("This will deploy the agent plugin <tt>mh_hpsa</tt> for monitoring the status of HP Raid controllers via hpacucli."),
-        choices = [
-            ( True, _("Deploy plugin for HP RAID controllers") ),
-            ( None, _("Do not deploy plugin for HP RAID controllers") ),
+        style = 'dropdown',
+        elements = [
+            Dictionary(
+                title = _("Deploy plugin for HP RAID controllers"),
+                elements = [
+                    ("interval", Age(
+                        title = _("Run asynchronously"),
+                        label = _("Interval for collecting data"),
+                        default_value = 300
+                    )),
+                ],
+            ),
+            FixedValue( None, title = _("Do not deploy plugin for HP RAID controllers") ),
         ]
     )
 )
