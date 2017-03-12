73a501783b0f05b236feb16a31ceae5f87120475
Robert Sander <gurubert@gurubert.de>
2017-03-12 15:56:03 +0100

    get ESX host for every VM

--- helper/vm_parent.py
+++ helper/vm_parent.py
@@ -7,31 +7,33 @@
 #
 
 import argparse
-import requests
+import checkmkapi
 import pprint
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--url', required=True, help='URL to Check_MK site')
 parser.add_argument('-u', '--username', required=True, help='name of the Automation user')
 parser.add_argument('-p', '--password', required=True)
-# parser.add_argument('-d', '--desc', required=False, help='Service Description to use', '
 args = parser.parse_args()
 
-if args.url[-1] == '/':
-    if not args.url.endswith('check_mk/'):
-        args.url += 'check_mk/'
-else:
-    if not args.url.endswith('check_mk'):
-        args.url += '/check_mk/'    
+mapi = checkmkapi.MultisiteAPI(args.url, args.username, args.password)
+wato = checkmkapi.WATOAPI(args.url, args.username, args.password)
 
-params = {
-    'output_format': 'json',
-    'service': 'ESX Hostsystem',
-    'view_name': 'servicedesc',
-    '_username': args.username,
-    '_secret': args.password,
-}
-              
-resp = requests.get(args.url + 'view.py', params=params)
-for state, icons, hostname, output, perfometer in resp.json():
-    print hostname, output
+resp = mapi.view(view_name='servicedesc', service='ESX Hostsystem')
+hosts = {}
+for item in resp:
+    if item['svc_plugin_output'].startswith('OK - Running on '):
+        hosts[item['host']] = item['svc_plugin_output'][16:]
+
+# pprint.pprint(hosts)
+
+changes = False
+for host in hosts.keys():
+    watohost = wato.get_host(host)
+    if watohost['attributes']['parents'] != [ hosts[host] ]:
+        print "%s gets %s as parent" % (host, hosts[host])
+        changes = True
+        # wato.edit_host(host, set_attr={'parents': [ hosts[host] ]})
+if changes:
+    # wato.activate()
+    pass
