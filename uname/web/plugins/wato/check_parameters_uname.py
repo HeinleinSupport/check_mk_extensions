3496f6c099bb09297f2b3f8bc1d882f0e28730f0
Robert Sander <r.sander@heinlein-support.de>
2017-05-08 14:36:26 +0200

    uname plugin

---
+++ uname/web/plugins/wato/check_parameters_uname.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+register_check_parameters(
+    subgroup_os,
+    "uname",
+    _("Kernel Version Check"),
+    Dictionary(
+        elements = [
+            ('release', RegExp( title = _("Expected Release") ) ),
+            ('version', RegExp( title = _("Expected Version") ) ),
+        ],
+        help = _('The check <tt>uname</tt> monitors the kernel version.'),
+    ),
+    None,
+    match_type = 'dict',
+)
+
