96fd904964898c6ac93e397ee580902b4288093a
Robert Sander <r.sander@heinlein-support.de>
2017-10-06 18:02:29 +0200

    first shot at apcaccess agent plugin

---
+++ apcaccess/web/plugins/metrics/apcaccess.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+metric_info["timeleft"] = {
+    "title" : _("Battery Runtime"),
+    "unit"  : "s",
+    "color" : "#80f000",
+}
