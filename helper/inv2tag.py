b2df9a95a449d0f3936b6cd28cbfaf37d1d8dfa9
Robert Sander <r.sander@heinlein-support.de>
2018-01-27 23:17:21 +0100

    set tag based on data from view

--- helper/inv2tag.py
+++ helper/inv2tag.py
@@ -8,7 +8,19 @@
 
 import argparse
 import checkmkapi
+import re
 import pprint
+
+tagmap = {
+    'inv_software_os_name': {
+        re.compile('centos', re.IGNORECASE): {'tag_opsys': 'redhat'},
+        re.compile('debian', re.IGNORECASE): {'tag_opsys': 'debian'},
+        re.compile('suse', re.IGNORECASE):   {'tag_opsys': 'suse'},
+        re.compile('ubuntu', re.IGNORECASE): {'tag_opsys': 'ubuntu'},
+#        re.compile('vmware', re.IGNORECASE): {'tag_opsys': 'esxi'},
+        re.compile('xenserver', re.IGNORECASE): {'tag_opsys': 'redhat'},
+    },
+}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--url', required=True, help='URL to Check_MK site')
@@ -20,11 +32,30 @@ mapi = checkmkapi.MultisiteAPI(args.url, args.username, args.password)
 wato = checkmkapi.WATOAPI(args.url, args.username, args.password)
 
 resp = mapi.view(view_name='opsys')
-result = {}
-for info in resp:
-    for key, value in info.iteritems():
-        if key not in result:
-            result[key] = set()
-        result[key].add(value)
 
-pprint.pprint(result)
+#
+# get uniq values from view
+#
+#result = {}
+#for info in resp:
+#    for key, value in info.iteritems():
+#        if key not in result:
+#            result[key] = set()
+#        result[key].add(value)
+#pprint.pprint(result)
+
+pprint.pprint(tagmap)
+
+for info in resp:
+    print info['host']
+    tags = {}
+    for attr, patterns in tagmap.iteritems():
+        if attr in info:
+            pprint.pprint(info[attr])
+            for pattern, settags in patterns.iteritems():
+                if pattern.search(info[attr]):
+                    tags.update(settags)
+    print tags
+
+    # wato.edit_host(info['host'], set_attr=tags)
+
