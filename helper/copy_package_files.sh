d6bdd7122f0928c855a59b77055fca52b47c6f30
Robert Sander <r.sander@heinlein-support.de>
2018-04-24 16:36:32 +0200

    use current directory as package name if no argument

--- helper/copy_package_files.sh
+++ helper/copy_package_files.sh
@@ -2,7 +2,13 @@
 
 # copy the files from a package to the CWD
 
-mkp list "$1" | while read file; do
+if [ -n "$1" ]; then
+  package="$1"
+else
+  package=$(basename $(pwd))
+fi
+
+mkp list "$package" | while read file; do
 
   lfile=${file#$OMD_ROOT/local/share/check_mk}
   lfile=${lfile#$OMD_ROOT/local}
