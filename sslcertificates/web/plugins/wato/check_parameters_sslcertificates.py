8e240692d4d5ae01f6ec0a7fb9cd96d61af64d1b
Robert Sander <r.sander@heinlein-support.de>
2018-07-05 14:21:33 +0200

    new option to ignore too old certs

--- sslcertificates/web/plugins/wato/check_parameters_sslcertificates.py
+++ sslcertificates/web/plugins/wato/check_parameters_sslcertificates.py
@@ -18,7 +18,15 @@ register_check_parameters(
              ListOfStrings(
                  title = _('Signature Algorithms that generate WARNs'),
                  default_value = ['md5WithRSAEncryption', 'sha1WithRSAEncryption']
-                 ))
+                 )),
+            ('ignore',
+             Tuple(
+                title = _('Ignore old Certificates'),
+                help = _('Set number of days after which an expired certificate is ignored. A reason has to be given.'),
+                elements = [
+                    Integer(title = _('Ignore after'), unit = _('days'), default_value = 365),
+                    TextAscii(title = _('Reason'), allow_empty = False, size = 72, min_len = 5),
+                ])),
         ],
     ),
     TextAscii(
