54dd097e3440bbc225d435ed39711caa733e16c0
Robert Sander <r.sander@heinlein-support.de>
2019-03-01 16:42:07 +0100

    Ricoh Printer used counter

---
+++ ricoh_used/web/plugins/metrics/ricoh_used.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+metric_info["pages_printed_cyan"] = {
+    "title" : _("Cyan Pages Printed"),
+    "unit"  : "count",
+    "color" : "34/a",
+}
+
+metric_info["pages_printed_magenta"] = {
+    "title" : _("Magenta Pages Printed"),
+    "unit"  : "count",
+    "color" : "12/a",
+}
+
+metric_info["pages_printed_yellow"] = {
+    "title" : _("Yellow Pages Printed"),
+    "unit"  : "count",
+    "color" : "23/a",
+}
+
+metric_info["pages_printed_black"] = {
+    "title" : _("Black Pages Printed"),
+    "unit"  : "count",
+    "color" : "51/a",
+}
+
+metric_info["pages_printed_other"] = {
+    "title" : _("Pages Printed"),
+    "unit"  : "count",
+    "color" : "52/a",
+}
+
+metric_info["cartridges_used_cyan"] = {
+    "title" : _("Cyan Cartridges Used"),
+    "unit"  : "count",
+    "color" : "34/a",
+}
+
+metric_info["cartridges_used_magenta"] = {
+    "title" : _("Magenta Cartridges Used"),
+    "unit"  : "count",
+    "color" : "12/a",
+}
+
+metric_info["cartridges_used_yellow"] = {
+    "title" : _("Yellow Cartridges Used"),
+    "unit"  : "count",
+    "color" : "23/a",
+}
+
+metric_info["cartridges_used_black"] = {
+    "title" : _("Black Cartridges Used"),
+    "unit"  : "count",
+    "color" : "51/a",
+}
+
+metric_info["cartridges_used_other"] = {
+    "title" : _("Cartridges Used"),
+    "unit"  : "count",
+    "color" : "52/a",
+}
