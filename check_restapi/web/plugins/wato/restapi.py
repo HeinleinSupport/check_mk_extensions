4d3ab5d389ec78d45518d672ab9a870ead27deff
Robert Sander <r.sander@heinlein-support.de>
2019-07-25 15:58:16 +0200

    check_restapi initial commit
    uses check_http to query REST APIs

---
+++ check_restapi/web/plugins/wato/restapi.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+#
+# (c) 2019 Heinlein Support GmbH
+#          Robert Sander <r.sander@heinlein-support.de>
+#
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  This file is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+register_rule("activechecks",
+              "active_checks:restapi",
+              Dictionary(
+                  title = _("Check REST API"),
+                  help = _("Checks REST API with <tt>check_http</tt>."),
+                  elements = [
+                      ('name', TextUnicode(
+                          title = _("Name"),
+                          help = _("Will be used in the service description. If the name starts with "
+                                   "a caret (<tt>^</tt>), the service description will not be prefixed with "
+                                  "<tt>REST API</tt>." ),
+                          allow_empty = False)),
+                      ('method', DropdownChoice(
+                          title = _("Method"),
+                          choices = [
+                              ('GET', 'GET'),
+                              ('HEAD', 'HEAD'),
+                              ],
+                          default_value = "GET",
+                          )),
+                      ( "virthost",
+                                    Tuple(
+                                        title = _("Virtual host"),
+                                        elements = [
+                                            TextAscii(
+                                                title = _("Name of the virtual host"),
+                                                help = _("Set this in order to specify the name of the "
+                                                         "virtual host for the query (using HTTP/1.1). If you "
+                                                         "leave this empty, then the IP address of the host "
+                                                         "will be used instead."),
+                                                allow_empty = False
+                                            ),
+                                            Checkbox(
+                                                label = _("Omit specifying an IP address"),
+                                                help = _("Usually Check_MK will nail this check to the "
+                                                         "IP address of the host it is attached to. With this "
+                                                         "option you can have the check use the name of the "
+                                                         "virtual host instead and do a dynamic DNS lookup."),
+                                                true_label = _("omit IP address"),
+                                                false_label = _("specify IP address"),
+                                            ),
+                                        ]
+                                    )
+                                ),
+                      ( "port", Integer(
+                          title = _("TCP Port"),
+                          minvalue = 1,
+                          maxvalue = 65535,
+                          default_value = 80
+                       )),
+                      ('ssl', Checkbox(
+                          title = _('SSL'),
+                          label = _('Use SSL and SNI'),
+                          default_value = True,
+                          )),
+                      ip_address_family_element,
+                      ('uri', TextAscii(
+                          title = _("URI to fetch (default is <tt>/</tt>)"),
+                          allow_empty = False,
+                          default_value = "/"
+                       )),
+                      ('auth', Alternative(
+                          title = _("Authentication"),
+                          elements = [
+                              IndividualOrStoredPassword(title = _("API Token")),
+                              Tuple(title = _("Credentials"),
+                                    elements = [
+                                        TextAscii(title=_("Username")),
+                                        IndividualOrStoredPassword(title=_("Password")),
+                                        ],
+                                    ),
+                              ])),
+                      ('header', ListOf(
+                          Tuple(
+                              title = _("Header"),
+                              elements = [
+                                  TextAscii(title = _("Name")),
+                                  TextAscii(title = _("Value"))
+                                  ],
+                              orientation = 'horizontal',
+                              ),
+                          title = _('Additional Headers'),
+                        )),
+                      ("expect_response_header", TextAscii(
+                          title = _("String to expect in response headers"),
+                          )),
+                      ( "expect_regex",
+                          Tuple(
+                              title = _("Regular expression to expect in content"),
+                              orientation = "vertical",
+                              show_titles = False,
+                              elements = [
+                                  RegExp(
+                                      label = _("Regular expression: "),
+                                      mode = RegExp.infix,
+                                  ),
+                                  Checkbox(label = _("Case insensitive")),
+                                  Checkbox(label = _("return CRITICAL if found, OK if not")),
+                                  Checkbox(label = _("Multiline string matching")),
+                              ]
+                          )),
+                      ],
+                      required_keys = ['name'],
+                  ),
+              match = 'all')
