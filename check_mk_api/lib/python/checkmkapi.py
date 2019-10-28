01645ebf367ce5a963c9b56e983c2d1b31114863
Robert Sander <r.sander@heinlein-support.de>
2019-10-28 16:59:40 +0100

    debug and unicode strings

--- check_mk_api/lib/python/checkmkapi.py
+++ check_mk_api/lib/python/checkmkapi.py
@@ -43,78 +43,80 @@ class WATOAPI():
             if fail:
                 print params['action']
                 pprint(data)
+                print resp.request.headers
+                print resp.request.body
                 import urllib
-                print "curl '%s?%s' -d 'request=%s'" % (self.api_url, urllib.urlencode(params), repr(data))
+                print "curl -v '%s?%s' -d \"request=%s\"" % (self.api_url, urllib.urlencode(params), repr(data))
                 raise RuntimeError('%s: %s' % ( errmsg, result['result'] ))
             else:
                 print '%s: %s' % ( errmsg, result['result'] )
         return result['result']
 
     def get_host(self, hostname, effective_attr=True):
-        api_get_host = { 'action': 'get_host', 'effective_attributes': 1 }
+        api_get_host = { u'action': u'get_host', u'effective_attributes': 1 }
         api_get_host.update(self.api_creds)
         if not effective_attr:
-            api_get_host['effective_attributes'] = 0
+            api_get_host[u'effective_attributes'] = 0
         return self.api_request(params=api_get_host,
-                                data={'hostname': hostname},
+                                data={u'hostname': hostname},
                                 errmsg='Error getting hostinfo for %s' % hostname)
 
     def get_all_hosts(self, effective_attr=True):
-        api_get_all_hosts = { 'action': 'get_all_hosts', 'effective_attributes': 1 }
+        api_get_all_hosts = { u'action': u'get_all_hosts', u'effective_attributes': 1 }
         api_get_all_hosts.update(self.api_creds)
         if not effective_attr:
-            api_get_all_hosts['effective_attributes'] = 0
+            api_get_all_hosts[u'effective_attributes'] = 0
         return self.api_request(params=api_get_all_hosts, errmsg='Error getting all hosts')
 
     def add_host(self, hostname, folder=None, set_attr = {}):
-        api_add_host = { 'action': 'add_host' }
+        api_add_host = { u'action': u'add_host' }
         api_add_host.update(self.api_creds)
         return self.api_request(params=api_add_host,
-                                data={'hostname': hostname,
-                                      'folder': folder,
-                                      'attributes': set_attr},
+                                data={u'hostname': hostname,
+                                      u'folder': folder,
+                                      u'attributes': set_attr},
                                 errmsg='Error adding host %s' % hostname)
 
     def edit_host(self, hostname, set_attr={}, unset_attr = [], nodes = []):
-        api_edit_host = { 'action': 'edit_host' }
+        api_edit_host = { u'action': u'edit_host' }
         api_edit_host.update(self.api_creds)
-        data = {'hostname': hostname}
+        data = {u'hostname': hostname}
         if set_attr:
-            data['attributes'] = set_attr
+            data[u'attributes'] = set_attr
         if unset_attr:
-            data['unset_attributes'] = unset_attr
+            data[u'unset_attributes'] = unset_attr
         if nodes:
-            data['nodes'] = nodes
+            data[u'nodes'] = nodes
         return self.api_request(params=api_edit_host,
                                 data=data,
                                 errmsg='Error updating host %s' % hostname)
 
     def delete_host(self, hostname):
-        api_del_host = { 'action': 'delete_host' }
+        api_del_host = { u'action': u'delete_host' }
         api_del_host.update(self.api_creds)
         return self.api_request(params=api_del_host,
-                                data={'hostname': hostname},
+                                data={u'hostname': hostname},
                                 errmsg='Error deleting host %s' % hostname)
     
     def disc_host(self, hostname, fail=False):
-        api_disc_host = { 'action': 'discover_services' }
+        api_disc_host = { u'action': u'discover_services' }
         api_disc_host.update(self.api_creds)
         return self.api_request(params=api_disc_host,
-                                data={"hostname": hostname},
+                                data={u'hostname': hostname},
                                 errmsg='Error discovering host %s' % hostname,
                                 fail=fail)
 
     def activate(self, sites=[]):
-        api_activate = { 'action': 'activate_changes'}
+        api_activate = { u'action': u'activate_changes'}
         api_activate.update(self.api_creds)
         if sites:
-            api_activate['mode'] = 'specific'
-            return self.api_request(params=api_activate, data={'sites': sites})
+            api_activate[u'mode'] = u'specific'
+            return self.api_request(params=api_activate, data={u'sites': sites})
         else:
             return self.api_request(params=api_activate)
 
     def bake_agents(self):
-        api_bake_agents = { 'action': 'bake_agents' }
+        api_bake_agents = { u'action': u'bake_agents' }
         api_bake_agents.update(self.api_creds)
         return self.api_request(params=api_bake_agents)
 
