7a600ff7fc5c5b8579ee12fc157b54f93b55e844
Robert Sander <r.sander@heinlein-support.de>
2020-01-23 11:23:47 +0100

    refactored check paramater ruleset definition

--- dovereplstat/web/plugins/wato/check_parameters_dovereplstat.py
+++ dovereplstat/web/plugins/wato/check_parameters_dovereplstat.py
@@ -1,38 +1,49 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-def dovereplstat_parameters(title):
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Tuple,
+)
+
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    CheckParameterRulespecWithoutItem,
+    rulespec_registry,
+)
+
+def _dovereplstat_parameters(title):
     return Tuple(title = title,
                  elements = [
                      Integer(title = "Warning at"),
                      Integer(title = "Critical at"),
                  ])
 
-register_check_parameters(
-    subgroup_applications,
-    "dovereplstat",
-    _("Dovecot Replication Status"),
-    Dictionary(
-        help = _('This check uses the output of `doveadm replicator status`.'),
-        elements = [
-            ('sync_requests',
-             dovereplstat_parameters(_('Levels for queued sync requests'))
-             ),
-            ('high_requests',
-             dovereplstat_parameters(_('Levels for queued high requests'))
-             ),
-            ('low_requests',
-             dovereplstat_parameters(_('Levels for queued low requests'))
-             ),
-            ('failed_requests',
-             dovereplstat_parameters(_('Levels for waiting failed requests'))
-             ),
-            ('full_resync_requests',
-             dovereplstat_parameters(_('Levels for queued full resync requests'))
-             ),
-        ]
-    ),
-    False,
-    match_type = "dict",
-)
-
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="dovereplstat",
+        group=RulespecGroupCheckParametersApplications,
+        match_type = "dict",
+        parameter_valuespec=lambda: Dictionary(
+            help = _('This check uses the output of `doveadm replicator status`.'),
+            elements = [
+                ('sync_requests',
+                 _dovereplstat_parameters(_('Levels for queued sync requests'))
+                 ),
+                ('high_requests',
+                 _dovereplstat_parameters(_('Levels for queued high requests'))
+                 ),
+                ('low_requests',
+                 _dovereplstat_parameters(_('Levels for queued low requests'))
+                 ),
+                ('failed_requests',
+                 _dovereplstat_parameters(_('Levels for waiting failed requests'))
+                 ),
+                ('full_resync_requests',
+                 _dovereplstat_parameters(_('Levels for queued full resync requests'))
+                 ),
+            ]
+        ),
+        title=lambda: _("Dovecot Replication Status"),
+    ))
