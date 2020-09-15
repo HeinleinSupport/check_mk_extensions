66cd2aa1a82555e044443da2b66324adc023a252
Robert Sander <r.sander@heinlein-support.de>
2020-09-15 13:11:39 +0200

    improves qcow2 provisioned space calculation

--- proxmox_provisioned/web/plugins/wato/agent_bakery_proxmox_provisioned.py
+++ proxmox_provisioned/web/plugins/wato/agent_bakery_proxmox_provisioned.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-from cmk.gui.valuespec import DropdownChoice
+from cmk.gui.valuespec import (
+    Alternative,
+    Dictionary,
+    FixedValue,
+)
 from cmk.gui.plugins.wato import (
     HostRulespec,
     rulespec_registry,
@@ -9,12 +13,22 @@ from cmk.gui.plugins.wato import (
 from cmk.gui.cee.plugins.wato.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
 
 def _valuespec_agent_config_proxmox_provisioned():
-    return DropdownChoice(
+    return Alternative(
         title = _("Proxmox Provisioned Storage (Linux)"),
         help = _("This will deploy the agent plugin <tt>proxmox_provisioned</tt> for monitoring storage space."),
-        choices = [
-            ( True, _("Deploy plugin for Proxmox Storage") ),
-            ( None, _("Do not deploy plugin for Proxmox Storage") ),
+        style = "dropdown",
+        elements = [
+            Dictionary(
+                title = _("Deploy plugin for Proxmox Storage"),
+                elements = [
+                    ("interval", Age(
+                        title = _("Run asynchronously"),
+                        label = _("Interval for collecting data"),
+                        default_value = 300
+                    )),
+                ],
+            ),
+            FixedValue(None, title = _("Do not deploy plugin for Proxmox Storage"), totext = _("(disabled)") ),
         ]
     )
 
