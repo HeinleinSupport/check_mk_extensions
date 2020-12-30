78b1ba76fa77df1acd7ceb8979854bf234725f6f
Robert Sander <r.sander@heinlein-support.de>
2020-12-30 14:18:01 +0100

    SSL Certificates WATO & Bakery Plugins

--- sslcertificates/web/plugins/wato/sslcertificates.py
+++ sslcertificates/web/plugins/wato/sslcertificates.py
@@ -1,11 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-register_check_parameters(
-    subgroup_applications,
-    "sslcertificates",
-    _("Parameters for SSL certificates"),
-    Dictionary(
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Tuple,
+    Integer,
+    ListOfStrings,
+    MonitoringState,
+    TextAscii,
+)
+
+from cmk.gui.plugins.wato import (
+    rulespec_registry,
+    CheckParameterRulespecWithItem,
+    RulespecGroupCheckParametersApplications,
+)
+
+def _item_spec_sslcertificates():
+    return TextAscii(
+        title = _("Certificate File"),
+        allow_empty = False,
+    )
+
+def _parameter_valuespec_sslcertificates():
+    return Dictionary(
         elements = [
             ('age', Tuple(
                 title = _('Certificate Age'),
@@ -28,11 +47,14 @@ register_check_parameters(
                     TextAscii(title = _('Reason'), allow_empty = False, size = 72),
                 ])),
         ],
-    ),
-    TextAscii(
-        title = _("Certificate File"),
-        allow_empty = False,
-    ),
-    'dict',
-)
+    )
 
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="sslcertificates",
+        group=RulespecGroupCheckParametersApplications,
+        item_spec=_item_spec_sslcertificates,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_sslcertificates,
+        title=lambda: _("Parameters for SSL certificates"),
+    ))
