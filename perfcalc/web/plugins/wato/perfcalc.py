ba9b2c8eeb4de17afa5714547446b983eb513bec
Robert Sander <r.sander@heinlein-support.de>
2021-03-15 17:12:12 +0100

    perfcalc for CMK 2.0

--- perfcalc/web/plugins/wato/perfcalc.py
+++ perfcalc/web/plugins/wato/perfcalc.py
@@ -1,47 +1,83 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-register_check_parameters(
-    subgroup_applications,
-    "perfcalc",
-    _("Calculated Performance Data Levels"),
-    ListOf(
-        Dictionary(
-            elements = [
-                ('dsname',
-                 TextAscii(title=_('Datasource Name'))
-                 ),
-                ('levels',
-                 Levels(title=_('Levels'))
-                 ),
-            ],
-            optional_keys = False,
-        ),
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Tuple,
+    Integer,
+    ListOfStrings,
+    MonitoringState,
+    TextAscii,
+)
+
+from cmk.gui.plugins.wato import (
+    rulespec_registry,
+    CheckParameterRulespecWithItem,
+    HostRulespec,
+    HostTagCondition,
+    Levels,
+    RulespecGroupCheckParametersApplications,
+)
+
+from cmk.gui.plugins.wato.datasource_programs import RulespecGroupDatasourceProgramsApps
+
+def _transform_perfcalc_parameter(params):
+    if isinstance(params, dict):
+        return params
+    return {'list': params}
+
+def _parameter_valuespec_perfcalc():
+    return Transform(Dictionary(
         title = _('Levels for Datasources'),
-        add_label = _('Add levels for datasource'),
+        optional_keys = None,
+        elements = [
+            ('list', ListOf(
+                Dictionary(
+                    elements = [
+                        ('dsname',       TextAscii(title=_('Datasource Name'))),
+                        ('levels',       Levels(title=_('Upper Levels'))),
+                        ('levels_lower', Levels(title=_('Lower Levels'))),
+                    ],
+                    optional_keys = ['levels', 'levels_lower'],
+                ),
+                add_label = _('Add levels for datasource'),
+            )),
+        ],
     ),
-    TextAscii(
+    forth = _transform_perfcalc_parameter,
+    )
+
+def _item_spec_perfcalc():
+    return TextAscii(
         title = _("Service descriptions"),
         help = _('Specify service descriptions of the host that uses the special agent "perfcalc".'),
         allow_empty = False
-    ),
-    match_type = "first",
-)
+    )
 
-def transform_perfcalc_ds(source):
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="perfcalc",
+        group=RulespecGroupCheckParametersApplications,
+        item_spec=_item_spec_perfcalc,
+        match_type="first",
+        parameter_valuespec=_parameter_valuespec_perfcalc,
+        title=lambda: _("Calculated Performance Data Levels"),
+    ))
+
+def _transform_perfcalc_ds(source):
     if 'hosttags' not in source:
         source['hosttags'] = []
     return source
 
-register_rule('datasource_programs',
-    "special_agents:perfcalc",
-    ListOf(
+def _valuespec_special_agents_perfcalc():
+    return ListOf(
         Transform(
         Dictionary(
             title = _("Service Check specification"),
             elements = [
                 ( "hosttags",
-                  watolib.HostTagCondition(
+                  HostTagCondition(
                       title = _('Hosttags'),
                       help = _('Select hosts by matching tags. Hosts will be searched on all connected sites. If you also enter host names below both will be combined with AND as usual.'),
                       allow_empty = True,
@@ -78,11 +114,19 @@ register_rule('datasource_programs',
             ],
             optional_keys = [ 'hosttags', 'host' ],
         ),
-        forth = transform_perfcalc_ds,
+        forth = _transform_perfcalc_ds,
         ),
         title = _('Service Specifications'),
         add_label = _('Add new service specification'),
-    ),
-    title = _("Calculate on the performance data of other checks."),
-    help  = _('This special agent uses performance data collected via livestatus and can calculate sums, minimums, maximums and averages of datasources. A service check is created for every service specification defined with this rule.'),
-    match = 'first')
+    )
+    
+    # help  = _('This special agent uses performance data collected via livestatus and can calculate sums, minimums, maximums and averages of datasources. A service check is created for every service specification defined with this rule.'),
+    # match = 'first')
+
+rulespec_registry.register(
+    HostRulespec(
+        group=RulespecGroupDatasourceProgramsApps,
+        name="special_agents:perfcalc",
+        valuespec=_valuespec_special_agents_perfcalc,
+        title = lambda: _("Calculate on the performance data of other checks."),
+    ))
