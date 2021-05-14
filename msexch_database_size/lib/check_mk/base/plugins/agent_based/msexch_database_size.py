87d085f068ba84377268fbe0409a445e34070aa8
Robert Sander <r.sander@heinlein-support.de>
2021-05-14 16:16:14 +0200

    msexch_database_size for CMK 2.0

---
+++ msexch_database_size/lib/check_mk/base/plugins/agent_based/msexch_database_size.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 vim: set ft=python:-*-
+
+# (c) 2020 Heinlein Support GmbH
+#          Robert Sander <r.sander@heinlein-support.de>
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+# {'DatabaseSize': '3.625 GB (3,892,314,112 bytes)', 'Name': 'deactivated_Users'}
+# {'DatabaseSize': '1.003 TB (1,102,732,853,248 bytes)', 'Name': 'active_Users'}
+# {'DatabaseSize': '41.63 GB (44,694,503,424 bytes)', 'Name': 'Shared_Mailbox'}
+
+# info = [[u'"Name"', u'"DatabaseSize"'],
+#         [u'"deactivated_Users"', u'"3.625 GB (3,892,314,112 bytes)"'],
+#         [u'"active_Users"', u'"1.003 TB (1,102,732,853,248 bytes)"'],
+#         [u'"Shared_Mailbox"', u'"41.63 GB (44,694,503,424 bytes)"']]
+
+from .agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+)
+
+from .agent_based_api.v1 import (
+    check_levels,
+    register,
+    render,
+    Service,
+    )
+
+def parse_msexch_database_size(string_table):
+    section = {}
+
+    for line in string_table:
+        name = line[0].strip('"')
+        if name == u'Name':
+            continue
+        sizestr = line[1].strip('"')
+        size = int(sizestr.split('(')[1].split(' ')[0].replace(',', ''))
+        section[name] = size
+
+    return section
+
+register.agent_section(
+    name="msexch_database_size",
+    parse_function=parse_msexch_database_size,
+)
+
+def discover_msexch_database_size(section) -> DiscoveryResult:
+    for instance, data in section.iteritems():
+        yield Service(item=instance)
+
+def check_msexch_database_size(item, params, section) -> CheckResult:
+    size = section.get(item)
+    if size is None:
+        return
+
+    yield from check_levels(size,
+                            metric_name='database_size',
+                            levels_upper=params,
+                            render_func=render.disksize,
+                            label="Size")
+
+register.check_plugin(
+    name="msexch_database_size",
+    service_name="Exchange Database Size %s",
+    sections=["msexch_database_size"],
+    discovery_function=discover_msexch_database_size,
+    check_function=check_msexch_database_size,
+    check_default_parameters={},
+    check_ruleset_name="msexch_database_size",
+)
--- msexch_database_size/web/plugins/wato/msexch_database_size.py
+++ msexch_database_size/web/plugins/wato/msexch_database_size.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato import (
-    HostRulespec,
     rulespec_registry,
     CheckParameterRulespecWithItem,
     RulespecGroupCheckParametersApplications,
@@ -15,31 +14,11 @@ from cmk.gui.valuespec import (
     TextAscii,
     Tuple,
 )
-from cmk.gui.cee.plugins.wato.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-
-def _valuespec_agent_config_msexch_database_size():
-    return DropdownChoice(
-        title = _("MS Exchange Database Size (Windows)"),
-        help = _("This will deploy the agent plugin <tt>msexch_database_size.ps1</tt> for monitoring the size of MS Exchange databases."),
-        choices = [
-            ( True, _("Deploy plugin for MS Exchange database size") ),
-            ( None, _("Do not deploy plugin for MS Exchange database size") ),
-        ]
-    )
-
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name="agent_config:msexch_database_size",
-        valuespec=_valuespec_agent_config_msexch_database_size,
-    ))
-
 
 def _item_spec_msexch_database_size():
     return TextAscii(
         title=_("Name of the database"),
     )
-
 
 def _parameter_valuespec_msexch_database_size():
     return Optional(
@@ -52,12 +31,11 @@ def _parameter_valuespec_msexch_database_size():
         title=_("Impose limits on the size of the database"),
     )
 
-
 rulespec_registry.register(
     CheckParameterRulespecWithItem(
         check_group_name="msexch_database_size",
         group=RulespecGroupCheckParametersApplications,
         item_spec=_item_spec_msexch_database_size,
         parameter_valuespec=_parameter_valuespec_msexch_database_size,
-        title=lambda: _("Size of MS Exchange databases"),
+        title=lambda: _("MS Exchange Database Size"),
     ))
