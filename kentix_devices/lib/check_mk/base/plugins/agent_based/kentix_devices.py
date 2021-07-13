297deaa79cd9059b1a9b1928bb3d5dd20d4ef327
Robert Sander <r.sander@heinlein-support.de>
2021-07-13 11:33:21 +0200

    moves check_humidity into check file
    no need for extra humidity extension

--- kentix_devices/lib/check_mk/base/plugins/agent_based/kentix_devices.py
+++ kentix_devices/lib/check_mk/base/plugins/agent_based/kentix_devices.py
@@ -37,9 +37,26 @@ from .utils.temperature import (
     check_temperature,
 )
 
-from .utils.humidity import check_humidity
-
 import time
+
+def check_humidity(humidity, params):
+    if isinstance(params, dict):
+        levels = ((params.get("levels") or (None, None)) + (params.get("levels_lower") or
+                                                            (None, None)))
+    elif isinstance(params, (list, tuple)):
+        # old params = (crit_low , warn_low, warn, crit)
+        levels = (params[2], params[3], params[1], params[0])
+    else:
+        levels = (None, None, None, None)
+
+    yield from check_levels(
+        humidity,
+        metric_name="humidity",
+        levels_upper=(levels[0], levels[1]),
+        levels_lower=(levels[2], levels[3]),
+        render_func=render.percent,
+        boundaries=(0, 100),
+    )
 
 def _get_dev_status_kentix_devices(alarm):
     alarm_state_map = { "1": State.CRIT,
