1c3954c476a18e0793eb802a62bef1805be6d320
Gerd Stolz <gerd.stolz@sva.de>
2021-11-25 11:13:43 +0100

    Add "AvailableNewSpaceForMailboxes" incl. relative thresholds to msesxch_database_size

--- msexch_database_size/web/plugins/wato/msexch_database_size.py
+++ msexch_database_size/web/plugins/wato/msexch_database_size.py
@@ -20,15 +20,36 @@ def _item_spec_msexch_database_size():
         title=_("Name of the database"),
     )
 
+def _transform_msexch_database_size(source):
+  if isinstance(source, dict):
+      return source
+  return {"size": source}
 def _parameter_valuespec_msexch_database_size():
-    return Optional(
-        Tuple(elements=[
-            Filesize(title=_("warning at")),
-            Filesize(title=_("critical at")),
-        ],),
-        help=_("The check will trigger a warning or critical state if the size of the "
-               "database exceeds these levels."),
-        title=_("Impose limits on the size of the database"),
+    return Transform(Dictionary(
+            optional_keys=['size','availSpace'],
+            title=_("Thresholds for total database size or percentage of free space within the database."),
+            help=_("Deleting old mailboxes leads to white space that MS Exchange administrators might want to clean up."),
+            elements = [('size',
+                Tuple(
+                    help=_("The check will trigger a warning or critical state if the size of the "
+                       "database exceeds these levels."),
+                    title=_("Impose limits on the size of the database"),
+                    elements=[
+                        Filesize(title=_("warning at")),
+                        Filesize(title=_("critical at")),
+                    ]),
+               ),
+               ('availSpace',
+                    Tuple(title=_('Percentage of free Mailbox space'),
+                                   elements=[
+                                       Percentage(title=_("Warning at")),
+                                       Percentage(title=_("Critical at")),
+                                   ],
+                    default_value=(20.0, 40.0),),
+               ),
+            ]
+        ),
+        forth=_transform_msexch_database_size,
     )
 
 rulespec_registry.register(
