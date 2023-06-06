511881e4aca40e87fea259ab79a1e85991abeee7
Robert Sander <r.sander@heinlein-support.de>
2023-06-06 16:14:48 +0200

    removes puppet_bakery

---
+++ helper/full_copy_package_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# copy the files from a package to the CWD
+
+if [ -n "$1" ]; then
+  package="$1"
+else
+  package=$(basename $(pwd))
+fi
+
+if ! mkp show "$package" > /dev/null; then
+  echo Package "$package" does not exist
+  exit 1
+fi
+
+mkp list "$package" | while read file; do
+
+  lfile=${file#$OMD_ROOT}
+  ldir=.$(dirname "$lfile")
+  mkdir -pv "$ldir"
+  cp -av "$file" "$ldir"
+
+done
