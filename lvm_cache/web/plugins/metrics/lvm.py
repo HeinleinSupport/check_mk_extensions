56c7cd21cb7c91d6a5e91035357efcce160677dd
Robert Sander <r.sander@heinlein-support.de>
2024-08-16 15:48:21 +0200

    removes deprecated extensions

--- lvm_cache/web/plugins/metrics/lvm.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from cmk.gui.plugins.metrics.translation import df_translation
-
-check_metrics["check_mk-lvm_vgs"] = df_translation
-check_metrics["check_mk-lvm_lvs"] = df_translation
--- perfcalc/web/plugins/metrics/perfcalc.py
+++
@@ -1,3 +0,0 @@
-check_metrics["check_mk-perfcalc"] = {
-    "wait" : { "name" : "io_wait" },
-}
