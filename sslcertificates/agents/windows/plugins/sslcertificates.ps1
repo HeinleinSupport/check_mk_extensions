8bf5724999aae17d5668b85202fc48ff413c3406
mueller-ma <mueller-ma@users.noreply.github.com>
2024-09-30 10:38:53 +0200

    [sslcertificates] De-duplicate code by using a loop
    See https://github.com/HeinleinSupport/check_mk_extensions/pull/167#issuecomment-2382452042

--- sslcertificates/agents/windows/plugins/sslcertificates.ps1
+++ sslcertificates/agents/windows/plugins/sslcertificates.ps1
@@ -1,36 +1,23 @@
-
 Write-Host '<<<sslcertificates:sep(0)>>>'
 
 $UnixEpoch = (Get-Date -Date "01/01/1970") ;
 
-foreach ($_ in Get-ChildItem -Recurse Cert:\LocalMachine\My) {
-  If ($_.DnsNameList) {$subject = $_.DnsNameList}
-  ElseIf ($_.Subject) {$subject = $_.Subject}
-  Else {$subject = $_.Thumbprint}
+$CertLocations = "Cert:\LocalMachine\My", "Cert:\CurrentUser\My"
 
-  $data = [ordered]@{
-    starts = (New-TimeSpan -Start $UnixEpoch -End $_.NotBefore).TotalSeconds ;
-    expires = (New-TimeSpan -Start $UnixEpoch -End $_.NotAfter).TotalSeconds ;
-    subj = $subject.Unicode ;
-    thumb = $_.Thumbprint ;
-    algosign = $_.SignatureAlgorithm.FriendlyName ;
+foreach ($CertLocation in $CertLocations) {
+  foreach ($_ in Get-ChildItem -Recurse $CertLocation) {
+    If ($_.DnsNameList) {$subject = $_.DnsNameList}
+    ElseIf ($_.Subject) {$subject = $_.Subject}
+    Else {$subject = $_.Thumbprint}
+
+    $data = [ordered]@{
+      starts = (New-TimeSpan -Start $UnixEpoch -End $_.NotBefore).TotalSeconds ;
+      expires = (New-TimeSpan -Start $UnixEpoch -End $_.NotAfter).TotalSeconds ;
+      subj = $subject.Unicode ;
+      thumb = $_.Thumbprint ;
+      algosign = $_.SignatureAlgorithm.FriendlyName ;
+    }
+
+    $data | ConvertTo-Json -Compress
   }
-
-  $data | ConvertTo-Json -Compress
-}
-
-foreach ($_ in Get-ChildItem -Recurse Cert:\CurrentUser\My) {
-  If ($_.DnsNameList) {$subject = $_.DnsNameList}
-  ElseIf ($_.Subject) {$subject = $_.Subject}
-  Else {$subject = $_.Thumbprint}
-
-  $data = [ordered]@{
-    starts = (New-TimeSpan -Start $UnixEpoch -End $_.NotBefore).TotalSeconds ;
-    expires = (New-TimeSpan -Start $UnixEpoch -End $_.NotAfter).TotalSeconds ;
-    subj = $subject.Unicode ;
-    thumb = $_.Thumbprint ;
-    algosign = $_.SignatureAlgorithm.FriendlyName ;
-  }
-
-  $data | ConvertTo-Json -Compress
 }
