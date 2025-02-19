57e4294bac0de3a77820ec1050f3178537f99217
Robert Sander <r.sander@heinlein-support.de>
2025-02-19 17:36:54 +0100

    refactors code into functions

--- sync_configuration/bin/sync_configuration.py
+++ sync_configuration/bin/sync_configuration.py
@@ -28,6 +28,8 @@ def get_aux_tags(wato, sync_tag):
         if aux_tag_data['id'].startswith(sync_tag):
             aux_tags[aux_tag_data['id']] = aux_tag_data['extensions']
             aux_tags[aux_tag_data['id']]['title'] = aux_tag_data['title']
+    if args.debug:
+        pprint(aux_tags)
     return aux_tags
 
 
@@ -40,16 +42,20 @@ def get_tag_groups(wato, sync_tag):
         if tag_group_data['id'].startswith(sync_tag):
             tag_groups[tag_group_data['id']] = tag_group_data['extensions']
             tag_groups[tag_group_data['id']]['title'] = tag_group_data['title']
+    if args.debug:
+        pprint(tag_groups)
     return tag_groups
 
 
 def get_rulesets(wato):
-    rulesets = []
+    rulesets = set()
     if args.verbose:
         print(f'getting rulesets from {url_to_site(wato._api_url)}')
     result, etag = wato.search_rulesets(folder='~')
     for ruleset_data in result.get('value', []):
-        rulesets.append(ruleset_data['id'])
+        rulesets.add(ruleset_data['id'])
+    if args.debug:
+        pprint(rulesets)
     return rulesets
 
 
@@ -92,8 +98,160 @@ def get_rules(wato, ruleset, sync_tag, central = True):
                 break
         if rule_relation:
             rule_relations[rule_ident] = rule_relation
+    if args.debug:
+        pprint(rules)
+        pprint(rule_relations)
     return rules, rule_relations
 
+
+def get_notification_rules(wato, sync_tag, central = True):
+    rules = {}
+    if args.verbose:
+        print(f'getting notification rules from {url_to_site(wato._api_url)}')
+    result, etag = wato.get_all_notification_rules()
+    if args.debug:
+        pprint(result)
+
+
+def sync_aux_tags(site_id, site_data, changes):
+    site_aux_tags = get_aux_tags(site_data['wato'], args.sync)
+    for aux_tag, aux_tag_data in central_aux_tags.items():
+        if aux_tag in site_aux_tags:
+            if aux_tag_data != site_aux_tags[aux_tag]:
+                if args.verbose:
+                    print(f'updating aux tag {aux_tag} on {site_id}')
+                site_data['wato'].edit_aux_tag(
+                    aux_tag,
+                    aux_tag_data.get('title'),
+                    aux_tag_data.get('topic'),
+                    aux_tag_data.get('help')
+                )
+                changes = True
+            del site_aux_tags[aux_tag]
+        else:
+            if args.verbose:
+                print(f'adding aux tag {aux_tag} to {site_id}')
+            site_data['wato'].create_aux_tag(
+                aux_tag,
+                aux_tag_data.get('title'),
+                aux_tag_data.get('topic'),
+                aux_tag_data.get('help')
+            )
+            changes = True
+    for site_aux_tag in site_aux_tags:
+        if args.verbose:
+            print(f'removing aux tag {site_aux_tag} from {site_id}')
+        site_data['wato'].delete_aux_tag(site_aux_tag)
+        changes = True
+    return changes
+
+
+def sync_tag_groups(site_id, site_data, changes):
+    site_tag_groups = get_tag_groups(site_data['wato'], args.sync)
+    for tag_group, tag_group_data in central_tag_groups.items():
+        if tag_group in site_tag_groups:
+            if tag_group_data != site_tag_groups[tag_group]:
+                if args.verbose:
+                    print(f'Updating tag group {tag_group} on {site_id}')
+                site_data['wato'].edit_host_tag_group(
+                    tag_group,
+                    '"*"',  # override etag
+                    tag_group_data.get('title'),
+                    tag_group_data.get('topic'),
+                    tag_group_data.get('help'),
+                    tag_group_data.get('tags'),
+                )
+                changes = True
+            del site_tag_groups[tag_group]
+        else:
+            if args.verbose:
+                print(f'Adding tag group {tag_group} to {site_id}')
+            site_data['wato'].create_host_tag_group(
+                tag_group,
+                tag_group_data.get('title'),
+                tag_group_data.get('tags'),
+                tag_group_data.get('topic'),
+                tag_group_data.get('help'),
+            )
+            changes = True
+    for site_tag_group in site_tag_groups:
+        if args.verbose:
+            print(f'removing tag group {site_tag_group} from {site_id}')
+        site_data['wato'].delete_host_tag_group(site_tag_group)
+        changes = True
+    return changes
+
+
+def sync_rules(site_id, site_data, changes):
+    site_rulesets = get_rulesets(site_data['wato'])
+    
+    if args.debug:
+        print(f"central_rulesets = {central_rulesets.union(site_rulesets)}")
+
+    for ruleset in central_rulesets.union(site_rulesets):
+        site_rules, site_relations = get_rules(
+            site_data['wato'],
+            ruleset,
+            args.sync,
+            central = False,
+        )
+        delete_rules = [x['id'] for x in site_rules.values()]
+        for rule_ident, rule_data in central_rules.get(ruleset, {}).items():
+            if rule_ident in site_rules:
+                if rule_data['ext'] != site_rules[rule_ident]['ext']:
+                    if args.verbose:
+                        print(f'updating rule "{rule_data["title"]}" in {ruleset} on {site_id}')
+                    if args.debug:
+                        print('rule_data')
+                        pprint(rule_data['ext'])
+                        pprint(site_rules[rule_ident]['ext'])
+                    site_data['wato'].edit_rule(
+                        site_rules[rule_ident]['id'],
+                        '"*"',
+                        rule_data['ext'].get('value_raw', ''),
+                        rule_data['ext'].get('condtions', {}),
+                        rule_data['ext'].get('properties', {}),
+                    )
+                    changes = True
+                delete_rules.remove(site_rules[rule_ident]['id'])
+            else:
+                if args.verbose:
+                    print(f'adding rule "{rule_data["title"]}" in {ruleset} to {site_id}')
+                properties = rule_data['ext'].get('properties', {})
+                m = regex_rule_title.match(rule_data['title'])
+                properties['description'] = f'{m.group(1)}[{args.sync}-{rule_ident}]{m.group(2)}'
+                site_rule, etag = site_data['wato'].create_rule(
+                    ruleset,
+                    rule_data['ext'].get('folder', '/'),
+                    rule_data['ext'].get('value_raw', ''),
+                    rule_data['ext'].get('conditions', {}),
+                    properties,
+                )
+                site_rules[rule_ident] = {
+                    'title': site_rule['title'],
+                    'ext': site_rule['extensions'],
+                    'id': site_rule['id'],
+                }
+                changes = True
+        for rule_ident, relation in central_relations.get(ruleset, {}).items():
+            site_relation = site_relations.get(rule_ident, {})
+            if relation.get('after') != site_relation.get('after'):
+                if args.verbose:
+                    print(f'moving rule "{central_rules[ruleset][rule_ident]["title"]}" ({site_rules[rule_ident]["id"]}) after "{central_rules[ruleset][relation["after"]]["title"]}" ({site_rules[relation["after"]]["id"]}) in {ruleset} on {site_id}')
+                site_data['wato'].move_rule(
+                    site_rules[rule_ident]['id'],
+                    '"*"',
+                    'after_specific_rule',
+                    neighbor_id=site_rules[relation['after']]['id'],
+                )
+                changes = True
+        for site_rule_id in delete_rules:
+            if args.verbose:
+                print(f'removing rule {site_rule_id} in {ruleset} from {site_id}')
+            site_data['wato'].delete_rule(site_rule_id)
+            changes = True
+        
+    return changes
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--url', help='URL to central Check_MK site')
@@ -164,17 +322,11 @@ for site in remove_sites:
 if args.debug:
     pprint(sites)
 
-aux_tags = get_aux_tags(central_wato, args.sync)
-if args.debug:
-    pprint(aux_tags)
+central_aux_tags = get_aux_tags(central_wato, args.sync)
 
-tag_groups = get_tag_groups(central_wato, args.sync)
-if args.debug:
-    pprint(tag_groups)
+central_tag_groups = get_tag_groups(central_wato, args.sync)
 
-central_rulesets = set(get_rulesets(central_wato))
-if args.debug:
-    pprint(central_rulesets)
+central_rulesets = get_rulesets(central_wato)
 
 central_rules = {}
 central_relations = {}
@@ -183,145 +335,17 @@ for ruleset in central_rulesets:
     if result1:
         central_rules[ruleset] = result1
         central_relations[ruleset] = result2
-if args.debug:
-    pprint(central_rules)
-    pprint(central_relations)
+
+# central_notification_rules = get_notification_rules(central_wato, args.sync)
 
 for site_id, site_data in sites.items():
     changes = False
 
-    site_aux_tags = get_aux_tags(site_data['wato'], args.sync)
-    for aux_tag, aux_tag_data in aux_tags.items():
-        if aux_tag in site_aux_tags:
-            if aux_tag_data != site_aux_tags[aux_tag]:
-                if args.verbose:
-                    print(f'updating aux tag {aux_tag} on {site_id}')
-                site_data['wato'].edit_aux_tag(
-                    aux_tag,
-                    aux_tag_data.get('title'),
-                    aux_tag_data.get('topic'),
-                    aux_tag_data.get('help')
-                )
-                changes = True
-            del site_aux_tags[aux_tag]
-        else:
-            if args.verbose:
-                print(f'adding aux tag {aux_tag} to {site_id}')
-            site_data['wato'].create_aux_tag(
-                aux_tag,
-                aux_tag_data.get('title'),
-                aux_tag_data.get('topic'),
-                aux_tag_data.get('help')
-            )
-            changes = True
-    for site_aux_tag in site_aux_tags:
-        if args.verbose:
-            print(f'removing aux tag {site_aux_tag} from {site_id}')
-        site_data['wato'].delete_aux_tag(site_aux_tag)
-        changes = True
+    changes = sync_aux_tags(site_id, site_data, changes)
+    
+    changes = sync_tag_groups(site_id, site_data, changes)
 
-    site_tag_groups = get_tag_groups(site_data['wato'], args.sync)
-    for tag_group, tag_group_data in tag_groups.items():
-        if tag_group in site_tag_groups:
-            if tag_group_data != site_tag_groups[tag_group]:
-                if args.verbose:
-                    print(f'Updating tag group {tag_group} on {site_id}')
-                site_data['wato'].edit_host_tag_group(
-                    tag_group,
-                    '"*"',  # override etag
-                    tag_group_data.get('title'),
-                    tag_group_data.get('topic'),
-                    tag_group_data.get('help'),
-                    tag_group_data.get('tags'),
-                )
-                changes = True
-            del site_tag_groups[tag_group]
-        else:
-            if args.verbose:
-                print(f'Adding tag group {tag_group} to {site_id}')
-            site_data['wato'].create_host_tag_group(
-                tag_group,
-                tag_group_data.get('title'),
-                tag_group_data.get('tags'),
-                tag_group_data.get('topic'),
-                tag_group_data.get('help'),
-            )
-            changes = True
-    for site_tag_group in site_tag_groups:
-        if args.verbose:
-            print(f'removing tag group {site_tag_group} from {site_id}')
-        site_data['wato'].delete_host_tag_group(site_tag_group)
-        changes = True
-
-    site_rulesets = set(get_rulesets(site_data['wato']))
-    if args.debug:
-        pprint(site_rulesets)
-
-    for ruleset in central_rulesets.union(site_rulesets):
-        site_rules, site_relations = get_rules(
-            site_data['wato'],
-            ruleset,
-            args.sync,
-            central = False,
-        )
-        if args.debug:
-            pprint(site_rules)
-            pprint(site_relations)
-        delete_rules = [x['id'] for x in site_rules.values()]
-        for rule_ident, rule_data in central_rules.get(ruleset, {}).items():
-            if rule_ident in site_rules:
-                if rule_data['ext'] != site_rules[rule_ident]['ext']:
-                    if args.verbose:
-                        print(f'updating rule "{rule_data["title"]}" in {ruleset} on {site_id}')
-                    if args.debug:
-                        print('rule_data')
-                        pprint(rule_data['ext'])
-                        pprint(site_rules[rule_ident]['ext'])
-                    site_data['wato'].edit_rule(
-                        site_rules[rule_ident]['id'],
-                        '"*"',
-                        rule_data['ext'].get('value_raw', ''),
-                        rule_data['ext'].get('condtions', {}),
-                        rule_data['ext'].get('properties', {}),
-                    )
-                    changes = True
-                delete_rules.remove(site_rules[rule_ident]['id'])
-            else:
-                if args.verbose:
-                    print(f'adding rule "{rule_data["title"]}" in {ruleset} to {site_id}')
-                properties = rule_data['ext'].get('properties', {})
-                m = regex_rule_title.match(rule_data['title'])
-                properties['description'] = f'{m.group(1)}[{args.sync}-{rule_ident}]{m.group(2)}'
-                site_rule, etag = site_data['wato'].create_rule(
-                    ruleset,
-                    rule_data['ext'].get('folder', '/'),
-                    rule_data['ext'].get('value_raw', ''),
-                    rule_data['ext'].get('conditions', {}),
-                    properties,
-                )
-                site_rules[rule_ident] = {
-                    'title': site_rule['title'],
-                    'ext': site_rule['extensions'],
-                    'id': site_rule['id'],
-                }
-                changes = True
-        for rule_ident, relation in central_relations.get(ruleset, {}).items():
-            site_relation = site_relations.get(rule_ident, {})
-            if relation.get('after') != site_relation.get('after'):
-                if args.verbose:
-                    print(f'moving rule "{central_rules[ruleset][rule_ident]["title"]}" ({site_rules[rule_ident]["id"]}) after "{central_rules[ruleset][relation["after"]]["title"]}" ({site_rules[relation["after"]]["id"]}) in {ruleset} on {site_id}')
-                site_data['wato'].move_rule(
-                    site_rules[rule_ident]['id'],
-                    '"*"',
-                    'after_specific_rule',
-                    neighbor_id=site_rules[relation['after']]['id'],
-                )
-                changes = True
-        for site_rule_id in delete_rules:
-            if args.verbose:
-                print(f'removing rule {site_rule_id} in {ruleset} from {site_id}')
-            site_data['wato'].delete_rule(site_rule_id)
-            changes = True
+    changes = sync_rules(site_id, site_data, changes)
 
     if changes:
         if args.verbose:
