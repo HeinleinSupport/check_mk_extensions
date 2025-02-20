7e4f274972569969f49d9aca8e422b7d27828aeb
Robert Sander <r.sander@heinlein-support.de>
2025-02-20 11:09:47 +0100

    adds translation for metric time to response_time

---
+++ imap/graphing/imap.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+#
+# (c) 2025 Heinlein Support GmbH
+#          Robert Sander <r.sander@heinlein-support.de>
+#
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  This file is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.graphing.v1 import translations
+
+translation_check_imap = translations.Translation(
+    name="check_imap",
+    check_commands=[
+        translations.ActiveCheck("imap")
+    ],
+    translations={
+        "time": translations.RenameTo("response_time"),
+    },
+)
