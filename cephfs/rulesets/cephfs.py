a9b410f35c96bc0279697ed771a7c0984869faf2
Robert Sander <r.sander@heinlein-support.de>
2025-05-07 10:17:41 +0200

    adds new extension to monitor mounted CephFS

---
+++ cephfs/rulesets/cephfs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# (c) 2025 Heinlein Consulting GmbH
+#          Robert Sander <r.sander@heinlein-support.de>
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2. This file is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.rulesets.v1 import (
+    Help,
+    Label,
+    Title,
+)
+from cmk.rulesets.v1.form_specs import (
+    BooleanChoice,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+)
+from cmk.rulesets.v1.rule_specs import (
+    AgentConfig,
+    Topic,
+)
+
+#   .--Bakery--------------------------------------------------------------.
+#   |                   ____        _                                      |
+#   |                  | __ )  __ _| | _____ _ __ _   _                    |
+#   |                  |  _ \ / _` | |/ / _ \ '__| | | |                   |
+#   |                  | |_) | (_| |   <  __/ |  | |_| |                   |
+#   |                  |____/ \__,_|_|\_\___|_|   \__, |                   |
+#   |                                             |___/                    |
+#   +----------------------------------------------------------------------+
+#   |                                                                      |
+#   '----------------------------------------------------------------------'
+#.
+
+def _valuespec_agent_config_cephfs():
+    return Dictionary(
+        elements = {
+            "deploy": DictElement(
+                required=True,
+                parameter_form=BooleanChoice(
+                    label=Label("Deploy plugin for cephfs"),
+                    prefill=DefaultValue(True),
+                ),
+            ),
+        },
+    )
+
+rule_spec_cephfs_bakery = AgentConfig(
+    name="cephfs",
+    title=Title("Cephfs mounts (Linux)"),
+    help_text=Help("This will deploy the agent plugin <tt>cephfs</tt> for monitoring these filesystems."),
+    topic=Topic.APPLICATIONS,
+    parameter_form=_valuespec_agent_config_cephfs,
+)
