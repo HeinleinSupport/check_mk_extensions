1c621275c0d73297144565240964225320b929de
Hagbear <hagbard@toco-tronic.de>
2025-07-31 21:50:43 +0200

    Update apcaccess.py
    add State.OK if the Self-Test is OK which is a Warning on the Orginal Code

--- apcaccess/agent_based/apcaccess.py
+++ apcaccess/agent_based/apcaccess.py
@@ -134,7 +134,10 @@ def check_apcaccess(item: str, params, section: Section) -> CheckResult:
             else:
                 yield Result(state=State.CRIT,
                              summary='Status is ' + data.get('STATUS'))
-        if 'SELFTEST' in data and data['SELFTEST'] != 'NO':
+        if 'SELFTEST' in data and data['SELFTEST'] == 'OK':
+            yield Result(state=State.OK,
+                         summary='Self-Test is ' + data['SELFTEST'])
+        else:
             yield Result(state=State.WARN,
                          summary='Self-Test is ' + data['SELFTEST'])
 
