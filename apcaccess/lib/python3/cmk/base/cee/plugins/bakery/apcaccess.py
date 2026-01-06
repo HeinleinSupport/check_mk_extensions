50b585c30f17253b6ad9d970cd75c57c521e2a7e
Robert Sander <r.sander@heinlein-support.de>
2026-01-06 10:06:10 +0100

    fixes bakery issue

--- apcaccess/lib/python3/cmk/base/cee/plugins/bakery/apcaccess.py
+++ apcaccess/lib/python3/cmk/base/cee/plugins/bakery/apcaccess.py
@@ -26,8 +26,8 @@ from cmk.base.plugins.bakery.bakery_api.v1 import (
 )
 
 def get_apcaccess_files(conf: Dict[str, Any]) -> FileGenerator:
-    if isinstance(conf, bool) and conf:
-        conf = {"deploy": True}
+    if isinstance(conf, bool):
+        conf = {"deploy": conf}
     if conf.get("deploy"):
         yield Plugin(base_os=OS.LINUX,
                     source=Path("apcaccess"))
