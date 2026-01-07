dd0621f8d500d75de4d18e0bed449a06e0479082
Robert Sander <r.sander@heinlein-support.de>
2026-01-07 17:01:39 +0100

    coniugo for Check API v2

--- notifications/coniugo/rulesets/coniugo.py
+++ notifications/coniugo/rulesets/coniugo.py
@@ -15,25 +15,70 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-register_notification_parameters("coniugo", Dictionary(
-    optional_keys = [],
-    elements = [
-        ("api_user", TextAscii(
-            title = _("Login"),
-            help = _("Configure the login name here"),
-            size = 40,
-            allow_empty = False,
-        )),
-        ("api_password", TextAscii(
-            title = _("API Password"),
-            help = _("You need to provide a valid API passowrd to be able to send notifications."),
-            size = 40,
-            allow_empty = False,
-        )),
-        ("api_url", TextAscii(
-            title = _("URL of SMS gateway"),
-            help = _("Set the URL of your SMS gateway in the form of http://IP:PORT"),
-            size = 40,
-        )),
-    ]
-))
+from cmk.rulesets.v1 import (
+    Help,
+    Title,
+)
+from cmk.rulesets.v1.form_specs import (
+    DictElement,
+    Dictionary,
+    Integer,
+    InputHint,
+    migrate_to_password,
+    Password,
+    String,
+    validators,
+)
+from cmk.rulesets.v1.rule_specs import (
+    NotificationParameters,
+    Topic,
+)
+
+def _migrate_password(model):
+    if isinstance(model, str):
+        model = ("password", model)
+    model = migrate_to_password(model)
+    return model
+
+def _valuespec_coniugo() -> Dictionary:
+    return Dictionary(
+        title = Title("Configure the ConiuGo connection"),
+        elements = {
+            "api_user": DictElement(
+                parameter_form=String(
+                    title = Title("Login"),
+                    help_text = Help("Configure the login name here"),
+                    field_size = 40,
+                )),
+            "api_password": DictElement(
+                parameter_form=Password(
+                    title = Title("API Password"),
+                    help_text = Help("You need to provide a valid API passowrd to be able to send notifications."),
+                    migrate=_migrate_password,
+                )),
+            "api_url": DictElement(
+                parameter_form=String(
+                    title = Title("URL of SMS gateway"),
+                    help_text = Help("Set the URL of your SMS gateway in the form of http://IP:PORT"),
+                    field_size = 60,
+                    prefill=InputHint(
+                        "https://host.domain.tld:port"
+                    ),
+                    custom_validate=[
+                        validators.Url(
+                            [
+                                validators.UrlProtocol.HTTP,
+                                validators.UrlProtocol.HTTPS,
+                            ],
+                        ),
+                    ],
+                )),
+        }
+    )
+
+rule_spec_notification_coniugo = NotificationParameters(
+    name="coniugo",
+    title=Title("SMS via ConiuGo gateway"),
+    parameter_form=_valuespec_coniugo,
+    topic=Topic.NOTIFICATIONS,
+)
