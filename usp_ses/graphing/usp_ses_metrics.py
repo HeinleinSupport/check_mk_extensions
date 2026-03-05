7b4184f4fb84a3f61a7de0b993f055b2804b021b
7meis <17747347+7meis@users.noreply.github.com>
2026-03-05 13:50:36 +0100

    make check cmk2.4 ready

---
+++ usp_ses/graphing/usp_ses_metrics.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""
+Check_MK metric definitions for USP SES
+
+Authors:    Roger Ellenberger <roger.ellenberger@wagner.ch>
+
+"""
+
+from cmk.graphing.v1 import Title
+from cmk.graphing.v1.metrics import (
+    Color, 
+    DecimalNotation,
+    Metric,
+    SINotation,
+)
+
+
+metric_avg_request_time = Metric(
+    name="avg_request_time",
+    title=Title("Average request time"),
+    unit=SINotation(symbol="s"),
+    color=Color.DARK_GRAY,
+)
+
+metric_active_users = Metric(
+    name="active_users",
+    title=Title("Active users"),
+    unit=DecimalNotation(),
+    color=Color.LIGHT_RED,
+)
+
+metric_client_connections = Metric(
+    name="client_connections",
+    title=Title("Client connections"),
+    unit=DecimalNotation(),
+    color=Color.ORANGE,
+)
