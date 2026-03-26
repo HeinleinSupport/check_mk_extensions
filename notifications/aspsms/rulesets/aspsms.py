e0c4ac3f8491188a4a093a850a9ba24d378080c7
Robert Sander <r.sander@heinlein-support.de>
2026-03-26 09:46:58 +0100

    aspsms for Check API v2

--- notifications/aspsms/rulesets/aspsms.py
+++ notifications/aspsms/rulesets/aspsms.py
@@ -15,29 +15,59 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-register_notification_parameters("aspsms", Dictionary(
-    optional_keys = ['originator'],
-    elements = [
-        ("user_key", TextAscii(
-            title = _("User Key"),
-            help = _("Configure the user key here. The key can be obtained from the "
-                     "<a href=\"https://www.aspsms.com/apicredentials/\" target=\"_blank\">"
-                     "aspsms.com</a> website."),
-            size = 40,
-            allow_empty = False,
-        )),
-        ("api_password", TextAscii(
-            title = _("API Password"),
-            help = _("You need to provide a valid API passowrd to be able to send notifications "
-                     "using ASPSMS. Register and login to <a href=\"https://www.aspsms.com/apicredentials/\" "
-                     "target=\"_blank\">ASPSMS</a> to obtain your API key."),
-            size = 40,
-            allow_empty = False,
-        )),
-        ("originator", TextAscii(
-            title = _("Originator"),
-            help = _("You can set the originator of the message here."),
-            size = 40,
-        )),
-    ]
-))
+from cmk.rulesets.v1 import (
+    Help,
+    Title,
+)
+from cmk.rulesets.v1.form_specs import (
+    DictElement,
+    Dictionary,
+    migrate_to_password,
+    Password,
+    String,
+)
+from cmk.rulesets.v1.rule_specs import (
+    NotificationParameters,
+    Topic,
+)
+
+def _migrate_password(model):
+    if isinstance(model, str):
+        model = ("password", model)
+    model = migrate_to_password(model)
+    return model
+
+def _valuespec_aspsms() -> Dictionary:
+    return Dictionary(
+        elements = {
+            "user_key": DictElement(
+                parameter_form=String(
+                    title = Title("User Key"),
+                    help_text = Help("Configure the user key here. The key can be obtained from the "
+                        "<a href=\"https://www.aspsms.com/apicredentials/\" target=\"_blank\">"
+                        "aspsms.com</a> website."),
+                    field_size = 40,
+                )),
+            "api_password": DictElement(
+                parameter_form=Password(
+                    title = Title("API Password"),
+                    help_text = Help("You need to provide a valid API passowrd to be able to send notifications "
+                        "using ASPSMS. Register and login to <a href=\"https://www.aspsms.com/apicredentials/\" "
+                        "target=\"_blank\">ASPSMS</a> to obtain your API key."),
+                    migrate=_migrate_password,
+                )),
+            "originator": DictElement(
+                parameter_form=String(
+                    title = Title("Originator"),
+                    help_text = Help("You can set the originator of the message here."),
+                    field_size = 40,
+                )),
+        }
+    )
+
+rule_spec_aspsms = NotificationParameters(
+    name="aspsms",
+    title=Title("SMS via aspsms.com"),
+    topic=Topic.NOTIFICATIONS,
+    parameter_form=_valuespec_aspsms,
+)
