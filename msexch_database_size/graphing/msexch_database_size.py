31fcab8ca1d1402742c5c6566fe8defab6006a4c
Sebastian Böhm <boehm107@gmail.com>
2026-08-10 17:27:45 +0200

    add graph definition for availableNewMailboxSpace

--- msexch_database_size/graphing/msexch_database_size.py
+++ msexch_database_size/graphing/msexch_database_size.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from cmk.graphing.v1 import Title
+from cmk.graphing.v1.graphs import Graph, MinimalRange
 from cmk.graphing.v1.metrics import Color, DecimalNotation, Metric, Unit
 
 
@@ -9,4 +10,13 @@ metric_msexch_available_new_mailbox_space = Metric(
     title=Title("Available New Mailbox Space"),
     unit=Unit(DecimalNotation("%")),
     color=Color.PURPLE,
+)
+
+graph_msexch_available_new_mailbox_space = Graph(
+    name="availableNewMailboxSpace",
+    title=Title("Available New Mailbox Space"),
+    minimal_range=MinimalRange(0, 100),
+    simple_lines=[
+        "availableNewMailboxSpace",
+    ],
 )
